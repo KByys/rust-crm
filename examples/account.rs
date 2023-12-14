e9e0fd90aa110912fbc219726824e9049d466022
KByys <3138859984@qq.com>
2023-12-14 16:17:18 +0800

    无关紧要的东西

--- examples/account.rs
+++ examples/account.rs
@@ -1,9 +1,17 @@
 use crm_rust::database::get_conn;
+use mysql::prelude::Queryable;
+use mysql_common::prelude::FromRow;
 
+#[derive(FromRow, Debug)]
+struct Test {
+    id: i32
+}
 
 fn main() -> mysql::Result<()> {
     let mut conn = get_conn()?;
-    
-
+    let sd: Test = conn.query_first("SELECT * FROM test1")?.unwrap();
+    println!(
+        "{:?}", sd
+    );
     Ok(())
 }

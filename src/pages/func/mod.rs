b5b959336b52c47e2bdb9b13d6bc8e15ce368867
KByys <3138859984@qq.com>
2023-12-19 18:55:15 +0800

    添加了sea的路由

--- src/pages/func/mod.rs
+++ src/pages/func/mod.rs
@@ -1,8 +1,9 @@
 mod appointment;
+mod sea;
 use axum::Router;
 
 mod customer;
 
 pub fn func_router() -> Router {
-    customer::customer_router()
+    customer::customer_router().merge(sea::sea_router())
 }

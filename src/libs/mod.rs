f8ad4479015c09fa98b3c8e05b65c7bb5ae2b0f3
KByys <3138859984@qq.com>
2023-12-22 16:55:30 +0800

    add parse multipart

--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -3,7 +3,10 @@ pub mod headers;
 pub mod perm;
 pub mod time;
 
+use axum::extract::Multipart;
 use base64::prelude::Engine;
+
+use crate::Response;
 /// base64 url safe encode
 pub fn base64_encode(input: impl AsRef<[u8]>) -> String {
     base64::prelude::BASE64_URL_SAFE_NO_PAD.encode(input)
@@ -23,4 +26,27 @@ macro_rules! do_if {
             $e
         }
     };
+}
+pub struct MessagePart {
+    pub files: Vec<(Vec<u8>, Option<String>)>,
+    pub data: String,
+}
+
+pub async fn parse_multipart(mut part: Multipart) -> Result<MessagePart, Response> {
+    let mut files = Vec::new();
+    let mut data = String::new();
+    while let Some(field) = part.next_field().await? {
+        match field.name() {
+            Some("file") => {
+                let filename = field.file_name().map(|s| s.to_owned());
+                let chunk = field.bytes().await?.to_vec();
+                files.push((chunk, filename));
+            }
+            Some("data") => {
+                data = field.text().await?;
+            }
+            _ => (),
+        }
+    }
+    Ok(MessagePart { files, data })
 }

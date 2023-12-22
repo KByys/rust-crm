c69403f0b6dfe78c508ba3ab7b984db0e1ea2933
KByys <3138859984@qq.com>
2023-12-22 13:56:16 +0800

    添加了产品的路由

--- src/pages/func/mod.rs
+++ src/pages/func/mod.rs
@@ -1,9 +1,12 @@
 mod appointment;
+mod product;
 mod sea;
 use axum::Router;
 
 mod customer;
 
 pub fn func_router() -> Router {
-    customer::customer_router().merge(sea::sea_router())
+    customer::customer_router()
+        .merge(sea::sea_router())
+        .merge(product::product_router())
 }

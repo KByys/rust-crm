52a8cf87616fac6ed27fe343d1c287bd4f86bc6a
KByys <3138859984@qq.com>
2023-12-27 22:36:32 +0800

    不重要

--- examples/account.rs
+++ examples/account.rs
@@ -1,6 +1,7 @@
 use crm_rust::database::get_conn;
 use mysql::prelude::Queryable;
 use mysql_common::prelude::FromRow;
+use serde_json::json;
 
 #[derive(FromRow, Debug)]
 struct Test {
@@ -8,6 +9,9 @@ struct Test {
 }
 
 fn main() -> mysql::Result<()> {
-    std::fs::write("te/good", b"Hello, world!")?;
+    let data = vec!["213", "3水电费交多少", "sdfdsfds"];
+    let d = json!(data);
+    println!("{}", d);
+    let dd: Vec<String> = serde_json::from_str(&d.to_string()).unwrap();
     Ok(())
 }

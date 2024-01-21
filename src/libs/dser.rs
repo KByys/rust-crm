cf0761ce84f87d5284469f6bd768a1c6271650fc
perju <pesju13@gmail.com>
2024-01-21 20:17:02 +0800

    add user struct's role serialize and deserialize

--- src/libs/dser.rs
+++ src/libs/dser.rs
@@ -1,8 +1,7 @@
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::fmt::Display;
 
-use crate::do_if;
-
+use crate::{do_if, perm::roles::ROLE_TABLES};
 
 pub fn deserialize_bool_to_i32<'de, D>(de: D) -> Result<i32, D::Error>
 where
@@ -35,4 +34,38 @@ where
         Some(value) => serializer.serialize_str(value),
         _ => serializer.serialize_str(""),
     }
+}
+#[macro_export]
+macro_rules! async_block {
+    ($asy:expr) => {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on($asy)
+    };
+}
+
+
+
+pub fn serialize_role<S>(id: &String, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let name = unsafe {
+        ROLE_TABLES.get_name(id).map_or(id.into(), |v|v.to_string())
+    };
+
+    serializer.serialize_str(&name)
+}
+
+pub fn deserialize_role<'de, D>(de: D) -> Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let name: String = Deserialize::deserialize(de)?;
+    let id = unsafe {
+        ROLE_TABLES.get_id(&name).map_or(name, |v|v.to_string())
+    };
+    Ok(id)
 }
